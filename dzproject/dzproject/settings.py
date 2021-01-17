be345cc98faf91c16fcc8296b50988cf9475309c
slmir <mirsonov43153@gmail.com>
2021-01-17 22:26:24 +0300

    init 1

--- dzproject/dzproject/settings.py
+++ dzproject/dzproject/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
